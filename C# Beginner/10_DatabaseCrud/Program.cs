8f0febddb0459d8b030cfe8340d0604614dc8c4b
Atlas <buse.arslan4386@gmail.com>
2026-05-15 16:33:42 +0300

    Add files via upload

---
+++ C# Beginner/10_DatabaseCrud/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _10_DatabaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crud --> Create, Read, Update, Delete
+
+            Console.WriteLine("***** Menü Sayfası İşlem Paneli *****");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------");
+
+            #region Kategori Ekleme İşlemi
+            //Console.Write("Eklemek istediğiniz kategori adı: ");
+            //string categoryName = Console.ReadLine();
+
+            ////sql ile bağlantı kurduk
+            //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-8HREKFC\\SQLEXPRESS; initial catalog=C#EgitimDb; integrated security=true");
+            //connection.Open();
+            ////string değişkenini sql tablosuna ekleme kodunu yazdık
+            //SqlCommand command = new SqlCommand("insert into TblCategory (CategoryName) values (@p1)", connection); //seçilen tablo bağlantıdaki veri tabanıyla çalıştığı için sonda connection dedik
+            //command.Parameters.AddWithValue("@p1", categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.WriteLine("Kategori başarıyla eklendi.");
+            #endregion
+
+            #region Ürün Ekleme İşlemi
+
+            //string productName;
+            //decimal productPrice;
+            ////bool productStatus;
+
+            //Console.Write("Ürün adı: ");
+            //productName = Console.ReadLine();
+            //Console.Write("Ürün fiyatı: ");
+            //productPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-8HREKFC\\SQLEXPRESS; initial catalog=C#EgitimDb; integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("insert into TblProduct (ProductName, ProductPrice, ProductStatus) values (@productName,@productPrice,@productStatus)", connection);
+            //command.Parameters.AddWithValue("@productName", productName);
+            //command.Parameters.AddWithValue("@productPrice", productPrice);
+            //command.Parameters.AddWithValue("@productStatus", true);
+            ////Değişiklikleri kaydet ve bunu veri tabanına yazdır demek için bu kodu yazıyoruz
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.WriteLine("Ürün eklemesi başarılı");
+
+            #endregion
+
+            #region Ürün Listeleme İşlemi
+
+            //SqlConnection connection = new SqlConnection("Data Source =DESKTOP-8HREKFC\\SQLEXPRESS;initial Catalog=C#EgitimDb;integrated security=true");
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("Select * From TblProduct", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command); //bizim için sql verilerini c# a çekecek olan köprümüz
+            //DataTable dataTable = new DataTable(); //geçici verileri hafızaya almak için kullandığımız sınıf
+            //adapter.Fill(dataTable); // adapter ın içini datatable dan gelen verilerle dolduracak
+            ////buranın öncesinde veriler alındı ve hazır şekilde bellekte bekliyor, şimdi verileri yazdırmamız gerek ve bunun kodunu da aşağıda yazacağız
+
+            ////datarow türünde değişken atadık, ve datatable ın her bir row unu tek tek okumamız lazım bu yüzden tüm satırı bir anda yazdırmak yerine, satır
+            ////içindeki bilgileri de almak için döngü içerisine başka bir döngü koyacağız
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //    foreach(var item in row.ItemArray)
+            //    {
+            //        Console.Write(item.ToString() + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //connection.Close();
+
+            #endregion
+
+            #region Ürün Silme İşlemi
+
+            //Console.Write("Silinecek ürün ID: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source =DESKTOP-8HREKFC\\SQLEXPRESS;initial Catalog=C#EgitimDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Delete From TblProduct Where ProductId=@productId", connection);
+            //command.Parameters.AddWithValue("productId", productId);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.WriteLine("Silme işlemi yapıldı.");
+
+            #endregion
+
+            #region Ürün Güncelleme İşlemi
+
+            //Console.Write("Güncellenecek ürün id: ");
+            //int productId = int.Parse(Console.ReadLine());
+            //Console.Write("Güncellenecek ürün adı: ");
+            //string productName = Console.ReadLine();
+            //Console.Write("Güncellenecek ürün fiyatı: ");
+            //decimal productPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source =DESKTOP-8HREKFC\\SQLEXPRESS;initial Catalog=C#EgitimDb;integrated security=true");
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("Update TblProduct Set ProductName=@productName, ProductPrice=@productPrice where ProductID=@productId", connection);
+            //command.Parameters.AddWithValue("@productName", productName);
+            //command.Parameters.AddWithValue("@productPrice", productPrice);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.WriteLine("Güncelleme Başarılı.");
+
+            #endregion
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ C# Beginner/10_DatabaseCrud/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("10_DatabaseCrud")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("10_DatabaseCrud")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("02ec0fc4-78c2-4193-b97b-5550eb332996")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
